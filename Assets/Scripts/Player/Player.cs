07ddb63b9fa3be450524c4aefa020970d0146014
Stefan Mihajlovic <stef.mihajlovicc@gmail.com>
2024-03-07 22:30:37 +0100

    Added HealthBar

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,8 +4,36 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public int maxHealth = 100;
+    public int currentHealth;
 
-    private void Awake()
+    public HealthBar healthBar;
+
+    private void Start()
     {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            TakeDamage(20);
+        }
+    }
+
+    private void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+
+        healthBar.SetHealth(currentHealth);
+    }
+
+    private void Heal(int amount)
+    {
+        currentHealth += amount;
+
+        healthBar.SetHealth(currentHealth);
     }
 }
