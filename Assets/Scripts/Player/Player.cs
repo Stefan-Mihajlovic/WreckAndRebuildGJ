34ab4603b4ea57a937e64cac24adaeca6681236d
Matke9 <matovicmihailo@gmail.com>
2024-03-07 21:43:51 +0100

    deleted inv

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] InventoryUI inventoryUI;
-
-    Inventory inventory;
 
     private void Awake()
     {
-        inventory = new Inventory();
-        inventory.addItem(new Item("sword", 1));
-        inventoryUI.SetInventory(inventory);
-        inventoryUI.RefreshInventory();
     }
 }
